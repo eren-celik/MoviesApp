7b0074de46d7bbec50a14e6c8bd42d5003e97cf9
erennlk <elik.dark91@icloud.com>
2021-06-09 17:59:58 +0300

    Initial Commit

---
+++ Movies App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies App
+//
+//  Created by Eren  Çelik on 9.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Movies AppTests/Movies_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Movies_AppTests.swift
+//  Movies AppTests
+//
+//  Created by Eren  Çelik on 9.06.2021.
+//
+
+import XCTest
+@testable import Movies_App
+
+class Movies_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
